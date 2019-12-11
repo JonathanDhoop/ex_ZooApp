8256aa635963a79993e73595229577d2ebf569e8
JonathanDhoop <jonathan.dhoop@student.howest.be>
2019-12-11 14:18:06 +0100

    tezst

--- Zoo.Lib/Lion.cs
+++ Zoo.Lib/Lion.cs
@@ -6,14 +6,20 @@ namespace Zoo.Lib
 {
     public enum Sex { F, M };
     public class Lion
-    {  
+    {
+        public int Age { get; set; }
         public Sex Sex { get; set; }
         public string Name { get; set; }
 
-        public Lion(string name, Sex sex)
+        public Lion(string name, Sex sex): this(name, sex, 0)
+        {
+
+        }
+        public Lion(string name, Sex sex, int age)
         {
             Name = name;
             Sex = sex;
+            Age = age;
         }
     }
 }
